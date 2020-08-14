214a7da2885fbfa70d89364329fdd4076393ed30
Alex Nguyen <alex.nguyen.3141@gmail.com>
2020-08-14 16:17:57 +1000

    Dockerized the new services

--- BackEnd/admin-service/src/main/java/edu/rmit/admin/controller/AdminController.java
+++ BackEnd/admin-service/src/main/java/edu/rmit/admin/controller/AdminController.java
@@ -7,9 +7,7 @@ import edu.rmit.admin.service.AdminService;
 import edu.rmit.common.errors.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -23,6 +21,11 @@ public class AdminController {
 
     @Autowired
     UserRepository userRepository;
+
+    @GetMapping("get")
+    public String getTest(){
+        return "GET";
+    }
 
     @PostMapping("/create")
     public ResponseEntity<?> registerABusinessOwner(RegistrationRequest registrationRequest){
