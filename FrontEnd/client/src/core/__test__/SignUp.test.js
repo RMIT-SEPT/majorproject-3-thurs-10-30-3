2da17196af52b59ca79841bf07d8deb3137e7ffb
Mitchell Gust s3782095 <s3782095@student.rmit.edu.au>
2020-09-16 22:01:36 +1000

    Format SignUp Test

--- FrontEnd/client/src/core/__test__/SignUp.test.js
+++ FrontEnd/client/src/core/__test__/SignUp.test.js
@@ -9,7 +9,7 @@ afterEach(cleanup)
 it('Sign up component renders correctly', () => {
     // Create Div Element
     const div = document.createElement("div");
-    // Render SignUp in Div Element
+    // Render SignUp with Div Element
     ReactDOM.render(<SignUp />, div);
 })
 
@@ -18,24 +18,24 @@ it('Sign up component is in document ? ', () => {
     // Is SignUp Container in the Document
     expect(
         getByTestId('signup-container'),
-      ).toBeInTheDocument()
+    ).toBeInTheDocument()
 })
 
-it('Sign up component has class name signup-container? ', () => {
+it('Sign up component has class name signup-container ? ', () => {
     const { getByTestId } = render(<SignUp />)
     // Does SignUp Container have the correct class
     expect(
         getByTestId('signup-container'),
-      ).toHaveClass('signup-container')
+    ).toHaveClass('signup-container')
 })
 
-it('Sign up component has class name signup-container? ', () => {
+it('Sign up component has class name signup-container ? ', () => {
     const { getByTestId } = render(<SignUp />)
     // Test Form contains valid Form Values
     expect(getByTestId('signup-form')).toHaveFormValues({
         username: '',
         email: '',
         password: "",
-      })
+    })
 })
 
