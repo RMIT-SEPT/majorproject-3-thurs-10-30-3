8b717c7cde3d84cc74c972b85b652f48f238817d
s3785894 <s3785894@student.rmit.edu.au>
2020-10-18 03:55:41 +1100

    Added negative unit tests for schedule cancel

---
+++ business/src/routes/__test__/cancel-schedule.test.ts
@@ -0,0 +1,48 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('returns HTTP Error Code 400 (Bad Request) when attempting to cancel a schedule that does not exist', async () => {
+    const response = await request(app)
+        .post('business/cancel/schedule/fakebusinessid')
+        .set("Cookie", global.signin())
+        .send({
+            scheduleId: 'fakescheduleid',
+            userId: 'fakeuserid'
+        });
+
+    expect(response.status).toEqual(400);
+});
+
+it('returns HTTP Error Code 400 (Bad Request) when attempting to cancel a schedule without supplying schedule id', async () => {
+    const response = await request(app)
+        .post('business/cancel/schedule/fakebusinessid')
+        .set("Cookie", global.signin())
+        .send({
+            userId: 'fakeuserid'
+        });
+
+    expect(response.status).toEqual(400);
+});
+
+it('returns HTTP Error Code 400 (Bad Request) when attempting to cancel a schedule without supplying user id', async () => {
+    const response = await request(app)
+        .post('business/cancel/schedule/fakebusinessid')
+        .set("Cookie", global.signin())
+        .send({
+            scheduleId: 'fakescheduleid'
+        });
+
+    expect(response.status).toEqual(400);
+});
+
+it('returns HTTP Error Code 401 (Unauthorised) when attempting to cancel a schedule without being logged in', async () => {
+    const response = await request(app)
+        .post('business/cancel/schedule/fakebusinessid')
+        .set("Cookie", global.signin())
+        .send({
+            scheduleId: 'fakescheduleid',
+            userId: 'fakeuserid'
+        });
+
+    expect(response.status).toEqual(401);
+});
